46120af9794817982855193eeaafeed946fd7e1b
Jawade Dhondiram Nivrati <dhondiram.jawade@yash.com>
2023-07-25 10:19:08 +0530

    cover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoverComponent } from './components/cover/cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule,
